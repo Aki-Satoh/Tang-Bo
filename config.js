b8a66f65f6b715fc603dc2bb2662f7add94c4689
Aki-Satoh <57630026+Aki-Satoh@users.noreply.github.com>
2021-06-03 13:23:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,29 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Gửi Mỹ Vân siêu xinh đẹp siêu cấp vũ trụ :3',
+    introDesc: `Mưa đan ngàn sợi bên đời
+Mưa không nhắn đủ một lời yêu em
+
+Nắng về cỏ lá đua chen
+Có làm ấm lại tình quên thuở nào
+
+Gió ơi cứ vẫn xạc xào
+Tim ngây thơ lắm thể nào cũng rung
+
+Đêm che một nỗi ngại ngùng
+Bâng quơ anh hỏi có cùng đường mơ
+
+Tình anh quấn quýt trong thơ
+Này em thế đã ngẩn ngơ động lòng?`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Yêu anh <33',
+    btnNo: 'Không :)',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'chiều nay 6h anh qua đón em nha công chúa. :3',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.messenger.com/t/100034234552894/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
