29cbb1a7ae08d9123de656573857eb7d62f10885
Aki-Satoh <57630026+Aki-Satoh@users.noreply.github.com>
2022-03-08 22:53:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,29 +1,16 @@
 const CONFIG = {
     introTitle: 'Gửi crush',
-    introDesc: `Mưa đan ngàn sợi bên đời
-Mưa không nhắn đủ một lời yêu em
-
-Nắng về cỏ lá đua chen
-Có làm ấm lại tình quên thuở nào
-
-Gió ơi cứ vẫn xạc xào
-Tim ngây thơ lắm thể nào cũng rung
-
-Đêm che một nỗi ngại ngùng
-Bâng quơ anh hỏi có cùng đường mơ
-
-Tình anh quấn quýt trong thơ
-Này em thế đã ngẩn ngơ động lòng?`,
+    introDesc: `Hello lại là tao đây, hôm nay cũng là ngày 8/3, t nghĩ đây cũng là thời điểm thích hợp để tỏ tình đó ạ :3`,
     btnIntro: '<3',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Làm người yêu anh nha?',
     btnYes: 'Yêu anh <33',
     btnNo: 'Không :)',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'chiều nay 6h anh qua đón em nha công chúa. :3',
-    btnAccept: 'Dạaaaaaaaaa <3',
+    mess: 'Biết nhau cũng gần 2 năm bây giờ mới có dịp tỏ tình em đó',
+    messDesc: 'Sang mess đi anh kể cho nghe từ lúc gặp em đến giờ thế nào ',
+    btnAccept: '<3',
     messLink: 'https://www.facebook.com/Thanhhau.2284' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
